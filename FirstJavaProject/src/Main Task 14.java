e27e02e2a1eb38769a5b1020f36304a2f9033692
OluwatosinOyagha <oluwatosin.mohammed@gmail.com>
2023-03-17 09:22:22 +0100

    Added Task 14 Introduction to Java showing my FirstJavaProject

---
+++ FirstJavaProject/src/Main Task 14.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first project");
+        System.out.println("I am excited about the project");
+        System.out.println("Thank you Testify Academy for the opportunity");
+        System.out.println("Software Testing is the way to go");
+        System.out.println("I am with TeamNewman where everything is new");
+        System.out.println("This is my very first introduction to Java");
+        System.out.println("I am a Software Tester");
+        System.out.println("I am learning so much this week already");
+        System.out.println("I have printed several lines on my console");
+    }
+}
